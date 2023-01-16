189634c83dc3787fa3446210d94b6fb5e4a0f339
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-16 15:57:16 -0300

    Validação do orçamento com o componente mensagem

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-  return (
+    const [mensaje, setMensaje] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje("Valor de orçamento inválido.")
+        } else {
+            // setMensaje('Valor de orçamento válido.')
+        }
+    }
+  
+  
+    return (
     <div className="contenedor-presupuesto contenedor sombra">
-        <form className="formulario">
+
+        <form onSubmit={handlePresupuesto} className="formulario">
             <div className="campo">
                 <label>Definir Orçamento</label>
 
@@ -16,6 +33,8 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
             </div>
 
             <input type="submit" value="Inserir"/>
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )
