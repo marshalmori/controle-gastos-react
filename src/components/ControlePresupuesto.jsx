7a7581899dd996d09c77e3d81d7616ce663dc099
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-18 11:48:20 -0300

    Calcular a porcentagem que foi gasta para inserir no gráfico

--- src/components/ControlePresupuesto.jsx
+++ src/components/ControlePresupuesto.jsx
@@ -1,46 +1,59 @@
 import { useEffect, useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
+const ControlePresupuesto = ({ gastos, presupuesto }) => {
+  const [porcentaje, setPorcentaje] = useState(0);
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
 
-const ControlePresupuesto = ({gastos, presupuesto}) => {
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+    const totalDisponible = presupuesto - totalGastado;
 
-    const [disponible, setDisponible] = useState(0);
-    const [gastado, setGastado] = useState(0);
+    //Cálculo porcentagem do gráfico
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
 
-    useEffect(() => {
-        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0);
-        const totalDisponible = presupuesto - totalGastado;
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
 
-        setGastado(totalGastado);
-        setDisponible(totalDisponible);
-    }, [gastos])
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje);
+    }, 1000);
+  }, [gastos]);
 
-
-    const formatearCantidad = (cantidad) => {
-        return cantidad.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
-    }
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
 
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-        <div>
-            <p>Aqui vai o gráfico</p>
-        </div>
+      <div>
+        <CircularProgressbar value={porcentaje} text={`${porcentaje}%`} />
+      </div>
 
-        <div className="contenido-presupuesto">
-            <p>
-                <span>Orçamento: </span> {formatearCantidad(presupuesto)}
-            </p>
-            <p>
-                <span>Disponível: </span> {formatearCantidad(disponible)}
-            </p>
-            <p>
-                <span>Gasto: </span> {formatearCantidad(gastado)}
-            </p>
-        </div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Orçamento: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponível: </span> {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gasto: </span> {formatearCantidad(gastado)}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ControlePresupuesto
+export default ControlePresupuesto;
